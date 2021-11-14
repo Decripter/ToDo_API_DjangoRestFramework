46aa0a12c096029111387888c80dbeda085fd744
Decripter <jeilsonaraujo@icloud.com>
2021-11-14 06:35:27 -0300

    feat: add the feature to get, change and delete specifc "todos"

--- to_do_django/app/views.py
+++ to_do_django/app/views.py
@@ -19,25 +19,25 @@ def todo_list(request):
             serializer.save()
             return Response(serializer.data, status = status.HTTP_201_CREATED)
         return Response(serializer.error_messages, status = status.HTTP_400_BAD_REQUEST)
-@api_view(['GET', 'PUT', 'DELETE'])
 
+@api_view(['GET', 'PUT', 'DELETE'])
 def todo_detail_change_and_delete(request, pk):
     try:
         todo = Todo.objects.get(pk=pk)
     except Todo.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
 
-    if request == 'GET':
+    if request.method == 'GET':
         serializer = TodoSerializer(todo)
         return Response(serializer.data)
 
-    elif request == 'PUT':
+    elif request.method == 'PUT':
         serializer = TodoSerializer(todo, data=request.data)
         if serializer.is_valid():
             return Response(serializer.data)
         return Response(serializer.error_messages, status=status.HTTP_400_BAD_REQUEST)
 
-    elif request == 'DELETE':
+    elif request.method == 'DELETE':
         todo.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
