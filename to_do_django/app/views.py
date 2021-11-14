bedb6365ba9918cf6641d3e085dfaf8c308ca6f6
Decripter <jeilsonaraujo@icloud.com>
2021-11-14 06:51:23 -0300

    fix: som bugs fixed, dont was recive get objects and dont was modifieding using the put method, now all ok

--- to_do_django/app/views.py
+++ to_do_django/app/views.py
@@ -34,6 +34,7 @@ def todo_detail_change_and_delete(request, pk):
     elif request.method == 'PUT':
         serializer = TodoSerializer(todo, data=request.data)
         if serializer.is_valid():
+            serializer.save()
             return Response(serializer.data)
         return Response(serializer.error_messages, status=status.HTTP_400_BAD_REQUEST)
 
