8d304c4f5f47c0d05146e45e14754233e6ffb508
Decripter <jeilsonaraujo@icloud.com>
2021-11-14 06:16:59 -0300

    conf: set response for mtehods GET, PUT, DELETE of todo_detail_change_and_delete from views.py

--- to_do_django/app/views.py
+++ to_do_django/app/views.py
@@ -1,5 +1,6 @@
 from django.http import response
 from rest_framework.decorators import api_view
+from rest_framework.serializers import Serializer
 from app.models import Todo
 from app.serializers import TodoSerializer
 from rest_framework.response import Response
@@ -25,9 +26,18 @@ def todo_detail_change_and_delete(request, pk):
         todo = Todo.objects.get(pk=pk)
     except Todo.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
-    if request == 'GET': pass
 
-    elif request == 'PUT': pass
+    if request == 'GET':
+        serializer = TodoSerializer(todo)
+        return Response(serializer.data)
 
-    elif request == 'DELETE': pass
+    elif request == 'PUT':
+        serializer = TodoSerializer(todo, data=request.data)
+        if serializer.is_valid():
+            return Response(serializer.data)
+        return Response(serializer.error_messages, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request == 'DELETE':
+        todo.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
